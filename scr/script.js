4ce127108ade5f8f452117dafe786fbe505425b4
yanakapylova <yana.kapylova@innowise.com>
2024-06-13 17:19:20 +0200

    popup

--- scr/script.js
+++ scr/script.js
@@ -1,44 +1,49 @@
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(showPosition);
+} else {
+  document.getElementById("location").innerHTML =
+    "Геолокация не поддерживается.";
+}
+
+function showPosition(position) {
+  //   var lat = position.coords.latitude;
+  //   var lon = position.coords.longitude;
+
+  var lat = 52.2288;
+  var lon = 20.98446;
+
+  grid = codegrid.CodeGrid();
+  grid.getCode(lat, lon, (error, code) => {
+    console.log(code);
+  });
+
+  //   var result = countryCoder.iso1A2Code([lat, lon]);
+
+//   document.querySelector("#total").innerHTML =
+//     "Широта: " + lat + "<br>Долгота: " + lon;
+}
+
 document.querySelector("#main button").addEventListener("click", () => {
   document.querySelector(".popupWrapper").style.display = "flex";
   document.querySelector(".bg").style.display = "block";
 
   console.log("bg");
 
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-      document.getElementById("location").innerHTML =
-        "Геолокация не поддерживается.";
-    }
-
-//   fetch("http://ipinfo.io/json")
-//     .then((r) => r.json())
-//     .then((r) => {
-//       console.log(r);
-//       let price;
-//       if (r.country == "PL") {
-//         price = 20;
-//         document.querySelector(".country").innerHTML = "Plan for Poland";
-//       } else if (r.country == "LT") {
-//         price = 15;
-//         document.querySelector(".country").innerHTML = "Plan for Lithuania";
-//       }
-//       document.querySelector(".number").innerHTML = price;
-//       document.querySelector("#total span").innerHTML = price * 1;
-//     });
-
-  function showPosition(position) {
-      var lat = position.coords.latitude;
-      var lon = position.coords.longitude;
-
-    // var lat = 52.2288;
-    // var lon = 20.98446;
-
-    var result = countryCoder.iso1A2Code([lat, lon]);
-    console.log(result);
-    document.getElementById("#total").innerHTML =
-      "Широта: " + lat + "<br>Долгота: " + lon;
-  }
+  //   fetch("http://ipinfo.io/json")
+  //     .then((r) => r.json())
+  //     .then((r) => {
+  //       console.log(r);
+  //       let price;
+  //       if (r.country == "PL") {
+  //         price = 20;
+  //         document.querySelector(".country").innerHTML = "Plan for Poland";
+  //       } else if (r.country == "LT") {
+  //         price = 15;
+  //         document.querySelector(".country").innerHTML = "Plan for Lithuania";
+  //       }
+  //       document.querySelector(".number").innerHTML = price;
+  //       document.querySelector("#total span").innerHTML = price * 1;
+  //     });
 
   document.querySelector(".bg").addEventListener("click", () => {
     console.log("bg");
