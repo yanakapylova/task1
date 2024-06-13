e45d0e787f7cdf36a10b84b89321fda71c286a9b
yanakapylova <yana.kapylova@innowise.com>
2024-06-13 17:41:01 +0200

    popup

--- scr/script.js
+++ scr/script.js
@@ -5,44 +5,24 @@ if (navigator.geolocation) {
 }
 
 function showPosition(position) {
-    var lat = position.coords.latitude;
-    var lon = position.coords.longitude;
-
-  //   var lat = 52.2288;
-  //   var lon = 20.98446;
-
-  let result = countryCoder.iso1A2Code([lon, lat]);
+  let result = countryCoder.iso1A2Code([position.coords.longitude, position.coords.latitude]);
 
   console.log(result);
 
-  //   grid = codegrid.CodeGrid();
-  //   grid.getCode(lat, lon, (error, code) => {
-  //     console.log(code);
-
-  //   });
+  if (result == "PL") {
+    price = 20;
+    document.querySelector(".country").innerHTML = "Plan for Poland";
+  } else if (r.country == "LT") {
+    price = 15;
+    document.querySelector(".country").innerHTML = "Plan for Lithuania";
+  }
+  document.querySelector(".number").innerHTML = price;
+  document.querySelector("#total span").innerHTML = price * 1;
 }
 
 document.querySelector("#main button").addEventListener("click", () => {
   document.querySelector(".popupWrapper").style.display = "flex";
   document.querySelector(".bg").style.display = "block";
-
-  console.log("bg");
-
-  //   fetch("http://ipinfo.io/json")
-  //     .then((r) => r.json())
-  //     .then((r) => {
-  //       console.log(r);
-  //       let price;
-  //       if (r.country == "PL") {
-  //         price = 20;
-  //         document.querySelector(".country").innerHTML = "Plan for Poland";
-  //       } else if (r.country == "LT") {
-  //         price = 15;
-  //         document.querySelector(".country").innerHTML = "Plan for Lithuania";
-  //       }
-  //       document.querySelector(".number").innerHTML = price;
-  //       document.querySelector("#total span").innerHTML = price * 1;
-  //     });
 
   document.querySelector(".bg").addEventListener("click", () => {
     document.querySelector(".bg").style.display = "none";
