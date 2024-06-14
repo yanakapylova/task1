96d359b33240d3eed1dfbe56e66cf7af86aef21f
yanakapylova <yana.kapylova@innowise.com>
2024-06-14 12:48:13 +0200

    location

--- scr/script.js
+++ scr/script.js
@@ -1,60 +1,66 @@
 let price = 15;
 
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(showPosition);
-} else {
-  console.log("Геолокация не поддерживается.");
-}
+// if (navigator.geolocation) {
+//   navigator.geolocation.getCurrentPosition(showPosition);
+// } else {
+//   console.log("Геолокация не поддерживается.");
+// }
 
-function showPosition(position) {
-  console.log(position.coords.longitude);
-  console.log(position.coords.latitude);
-  let result = countryCoder.iso1A2Code([
-    position.coords.longitude,
-    position.coords.latitude,
-  ]);
+// function showPosition(position) {
+//   console.log(position.coords.longitude);
+//   console.log(position.coords.latitude);
+//   let result = countryCoder.iso1A2Code([
+//     position.coords.longitude,
+//     position.coords.latitude,
+//   ]);
 
-  console.log(result);
+//   console.log(result);
 
-  if (result == "PL") {
-    price = 20;
-    document.querySelector(".country").innerHTML = "Plan for Poland";
-  } else if (result == "LT") {
-    price = 15;
-    document.querySelector(".country").innerHTML = "Plan for Lithuania";
-  }
-  document.querySelector(".number").innerHTML = price;
-  document.querySelector("#total span").innerHTML = price * 1;
-}
+//   if (result == "PL") {
+//     price = 20;
+//     document.querySelector(".country").innerHTML = "Plan for Poland";
+//   } else if (result == "LT") {
+//     price = 15;
+//     document.querySelector(".country").innerHTML = "Plan for Lithuania";
+//   }
+//   document.querySelector(".number").innerHTML = price;
+//   document.querySelector("#total span").innerHTML = price * 1;
+// }
 
 const buttonSubmit = document.querySelector(".popupWrapper form button#submit");
 const popupWrapper = document.querySelector(".popupWrapper");
-const bg = document.querySelector(".bg");
 
 // для теста
-// price = 20;
-// document.querySelector(".country").innerHTML = "Plan for Poland";
-// document.querySelector(".number").innerHTML = price;
-// document.querySelector("#total span").innerHTML = price * 1;
+price = 20;
+document.querySelector(".country").innerHTML = "Plan for Poland";
+document.querySelector(".number").innerHTML = price;
+document.querySelector("#total span").innerHTML = price * 1;
 
 document.querySelector("#main button").addEventListener("click", () => {
   popupWrapper.style.display = "flex";
-  bg.style.display = "block";
 
-  bg.addEventListener("click", () => {
-    bg.style.display = "none";
+  const hidePopup = function () {
+    popupWrapper.style.display = "none";
+  };
+
+  document.querySelector(".popupWrapper").addEventListener("click", () => {
     popupWrapper.style.display = "none";
   });
 
-  document.querySelector(".popupWrapper button#buy").addEventListener("click", () => {
-    bg.style.display = "none";
-    popupWrapper.style.display = "none";
-  });
+  document
+    .querySelector(".popupWrapper .popup")
+    .addEventListener("click", (e) => {
+      e.stopImmediatePropagation();
+      console.log("stopped");
+    });
 
-  document.querySelector(".popupWrapper .box .close").addEventListener("click", () => {
-    bg.style.display = "none";
-    popupWrapper.style.display = "none";
-  });
+  document
+    .querySelector(".popupWrapper button#buy")
+    .addEventListener("click", hidePopup);
+
+  document
+    .querySelector(".popupWrapper .box .close")
+    .addEventListener("click", hidePopup);
 });
 
 $(document).on(".popupWrapper form input", function (ev) {
